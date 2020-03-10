26f24f0a2baa1c5a71f5e15beef8c5d1a9f613e5
Luis Alvarez <alvarezcl@alumni.stanford.edu>
2020-03-10 14:52:59 -0700

    changed hostname and egress options

--- ec2/terraform/main.tf
+++ ec2/terraform/main.tf
@@ -43,6 +43,13 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 output "public_ip" {
