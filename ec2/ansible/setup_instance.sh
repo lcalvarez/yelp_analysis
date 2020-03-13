48839e03384ff85cfd74d5f780028684c74f91a2
Luis Alvarez <alvarezcl@alumni.stanford.edu>
2020-03-12 17:33:45 -0700

    finalizing

--- ec2/ansible/setup_instance.sh
+++ ec2/ansible/setup_instance.sh
@@ -16,3 +16,5 @@ ansible-playbook yelp_data.yml -i hosts -b
 # Install jupyterhu
 ansible-playbook jupyter.yml -i hosts -b
 
+# Download jupyter notebook with data analysis
+ansible-playbook download_jupyter_notebook.yml -i hosts -b
